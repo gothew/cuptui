f4d474cba7d1770aee491be2357758fef0d3b762
Стив <kenethra.74@gmail.com>
2022-08-17 21:01:32 -0600

    init repo

---
+++ dirfs/dirfs.go
@@ -0,0 +1,18 @@
+package dirfs
+
+import (
+	"errors"
+	"os"
+)
+
+// CreateDirectory new directories given a name
+func CreateDirectory(name string) error {
+	if _, err := os.Stat(name); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(name, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karchx/cuptui
+
+go 1.18
