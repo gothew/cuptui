2033dfb8e9883a33b3c6b65f9a7b2b6bb0451080
Стив <kenethra.74@gmail.com>
2022-08-28 13:31:38 -0600

    feat: rename directory func

--- dirfs/dirfs.go
+++ dirfs/dirfs.go
@@ -5,6 +5,13 @@ import (
 	"os"
 )
 
+// RenameDirectoryItem renames a directory or files given a source and
+// destination.
+func RenameDirectoryItem(src, dst string) error {
+  err := os.Rename(src, dst)
+  return err
+}
+
 // CreateDirectory new directories given a name
 func CreateDirectory(name string) error {
 	if _, err := os.Stat(name); errors.Is(err, os.ErrNotExist) {
