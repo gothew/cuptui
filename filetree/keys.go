cd56b60caac13203d4bac35f9726c5147efbfd7a
Стив <kenethra.74@gmail.com>
2022-11-13 12:59:32 -0600

    style: format

--- filetree/keys.go
+++ filetree/keys.go
@@ -3,7 +3,7 @@ package filetree
 import "github.com/charmbracelet/bubbles/key"
 
 var (
-  openDirectoryKey = key.NewBinding(key.WithKeys(" "), key.WithHelp("space", "open directory"))
+	openDirectoryKey   = key.NewBinding(key.WithKeys(" "), key.WithHelp("space", "open directory"))
 	createDirectoryKey = key.NewBinding(key.WithKeys("N"), key.WithHelp("N", "create directory"))
 	createFileKey      = key.NewBinding(key.WithKeys("n"), key.WithHelp("n", "create file"))
 	renameItemKey      = key.NewBinding(key.WithKeys("r"), key.WithHelp("r", "rename item"))
